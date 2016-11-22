55b38c0118c3defec3fb86bb136aea8e90b238d7
cde-v <cpdevill@gmail.com>
2016-11-22 00:20:14 -0500

    init2

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var routes = require('server/routes');
 
+console.log(process.env.PORT);
+
 var PORT = process.env.PORT || 3000;
+
+console.log(process.env.PORT);
 
 app.use(bodyParser.json());
 
@@ -17,6 +21,10 @@ app.all('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+console.log(process.env.PORT);
+
 app.listen(PORT, function() {
     console.log('Server running on ' + PORT);
 });
+
+console.log(process.env.PORT);
