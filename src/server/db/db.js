65efd8ddd65f2994e28282a2c122bda4f30601a2
cde-v <cpdevill@gmail.com>
2016-11-22 00:40:54 -0500

    init4

--- src/server/db/db.js
+++ src/server/db/db.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/todos');
+// mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/todos');
+mongoose.connect(process.env.MONGOLAB_URI);
 
 var Todo = mongoose.model('Todo', {
     task: String,
