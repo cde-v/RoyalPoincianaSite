357b07662adaf12c9867a1bd31e26b7b80684b84
cde-v <cpdevill@gmail.com>
2016-11-28 20:56:44 -0500

    href fix

--- src/browser/common/common.js
+++ src/browser/common/common.js
@@ -1,12 +1,10 @@
 import angular from 'angular';
 import Navbar from './navbar/navbar';
 import Footbar from './footbar/footbar';
-import Hero from './hero/hero';
 
 let commonModule = angular.module('app.common', [
   Navbar.name,
-  Footbar.name,
-  Hero.name
+  Footbar.name
 ]);
 
 export default commonModule;
