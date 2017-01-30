7a572988e8fc9e42b8d35eeadb4b1254e6527b63
cde-v <cpdevill@gmail.com>
2017-01-29 20:13:08 -0500

    admin

--- app/server/models/User.js
+++ app/server/models/User.js
@@ -16,7 +16,7 @@ var userSchema = new mongoose.Schema({
   passwordResetToken: String,
   passwordResetExpires: Date,
   ownerOrRenter: String,
-  isAdmin: { type: Boolean, default: false }
+  isAdmin: { type: Boolean, default: true }
 }, schemaOptions);
 
 userSchema.pre('save', function(next) {
